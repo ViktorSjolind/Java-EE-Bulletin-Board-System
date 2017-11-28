6cc2d88b986072b99b3d976a8ba0d7c1741446b0
ViktorSjolind <viktor.sjolind@gmail.com>
2017-11-28 21:29:40 +0200

    OP post linked to list of posts

--- src/main/java/webapp/DatabaseManager.java
+++ src/main/java/webapp/DatabaseManager.java
@@ -16,16 +16,18 @@ public class DatabaseManager {
 	private String PASSWORD = "t00rSQL";	
 	private Connection connection = null;
 	
-	private PreparedStatement selectThread = null;
+	private PreparedStatement selectThreads = null;
 	private PreparedStatement insertThread = null;
 	private PreparedStatement selectPosts = null;
+	private PreparedStatement selectSpecificThread = null;
 	private ResultSet resultSet = null;
 	
 	public DatabaseManager(){
 		try{
 			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-			selectThread = connection.prepareStatement("SELECT id, content, date FROM thread");
-			selectPosts = connection.prepareStatement("SELECT id, content, date FROM post where post.parent_id = ?");
+			selectThreads = connection.prepareStatement("SELECT id, content, date FROM thread");
+			selectSpecificThread = connection.prepareStatement("SELECT id, content, date FROM thread WHERE thread.id = ?");			
+			selectPosts = connection.prepareStatement("SELECT id, content, date FROM post WHERE post.parent_id = ?");			
 			insertThread = connection.prepareStatement("insert into thread values(0, ?, ?)");
 		}catch(SQLException e){
 			e.printStackTrace();
@@ -47,7 +49,7 @@ public class DatabaseManager {
 	public List getThreadList(){
 		List<Thread> result = new ArrayList();
 		try{
-			resultSet = selectThread.executeQuery();
+			resultSet = selectThreads.executeQuery();
 			while(resultSet.next()){
 				result.add(new Thread(resultSet.getInt("id"), resultSet.getString("content"), resultSet.getTimestamp("date")));
 			}
@@ -58,12 +60,29 @@ public class DatabaseManager {
 		return result;
 	}
 	
+	public List getSpecificThread(int ID){		
+		List<Thread> result = new ArrayList();
+		try{
+			selectSpecificThread.setInt(1, ID);
+			System.out.println("query: " + selectSpecificThread);			
+			//Fetch the OP post
+			resultSet = selectSpecificThread.executeQuery();
+			while(resultSet.next()){
+				result.add(new Thread(resultSet.getInt("id"), resultSet.getString("content"), resultSet.getTimestamp("date")));
+			}
+			
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 	public List getPostsList(int parentID){
 		//reusing Thread class for posts, same DB structure
 		List<Thread> result = new ArrayList();
 		try{
 			selectPosts.setInt(1, parentID);
-			System.out.println("query: " + selectPosts);
+			System.out.println("query: " + selectPosts);				
 			resultSet = selectPosts.executeQuery();
 			while(resultSet.next()){
 				result.add(new Thread(resultSet.getInt("id"), resultSet.getString("content"), resultSet.getTimestamp("date")));
